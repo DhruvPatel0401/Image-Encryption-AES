c0dcfdc0eeba86a86f33a93151cd0890f140871e
Dhruv Patel <pateldhruv130401@gmail.com>
2024-04-20 16:55:32 -0400

    deleted old data

--- app_image_encrpyt/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class EncryptedImage(models.Model):
-    encrypted_image = models.ImageField(upload_to='media/', null=True, blank=True)
-    password = models.CharField(max_length=256)
-
